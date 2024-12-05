3ff2aa9c67fe13a3265ee2e76ce5aac46635514c
susana0994 <susypao09@gmail>
2024-12-04 21:16:09 -0300

    Controlador Informes

--- controlador/Controlador_informes.py
+++ controlador/Controlador_informes.py
@@ -4,4 +4,13 @@ from Controlador_informes import mostrar_empleados
 
 def mostrar_empleados(Informe):
     conn=conectar()
-    
+    try:
+        if conn is not None:
+            cursor=conn.cursor()
+            cursor.execute("SELECT id, nombre, apellido, direccion, telefono, email, fecha_inicio_contrato, salario FROM empleados")
+            empleados_encontrados=cursor.fetchall()
+            empleados=[]
+            if len(empleados_encontrados)>0:
+                for empleado in empleados_encontrados:
+                    empleado_encontrado=Empleado()
+                    
