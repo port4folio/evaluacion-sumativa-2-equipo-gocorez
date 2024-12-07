59aa4c64cf6b1934943204bf924c0686735448c0
LeoCofre <leocofre1981@gmail.com>
2024-12-07 13:34:16 -0300

    Se agrega metodo mostrar departamento

--- controlador/Controlador_departamento.py
+++ controlador/Controlador_departamento.py
@@ -1,6 +1,7 @@
 from modelo.ConexionBD import conectar
 from modelo.Departamento import Departamento
 
+
 def agregar_departamento(departamento):
     conn=conectar()
     try:
@@ -36,11 +37,48 @@ def editar_departamento(departamento):
         conn.close()
 
 
-def buscar_departamento():
-    pass
+def buscar_departamento(nombre):
+    conn=conectar()
+    try:
+        if conn is not None:
+            cursor=conn.cursor()
+            cursor.execute(
+                "SELECT id,nombre,gerente FROM departamento WHERE nombre=%s",
+                (nombre,)
+                )
+            departamento=cursor.fetchone()
+            if departamento is not None:
+                departemento_encontrado=Departamento(departamento[1],departamento[2],departamento[3])
+                departemento_encontrado.set_id(departamento[0])
+            else:
+                departemento_encontrado=None
+            return departemento_encontrado
+        else:
+            return None
+    except Exception as e:
+        print(f"Error al conectar. {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 def eliminar_departamento():
     pass
 
 def mostrar_departamentos():
-    pass
+    conn = conectar()  # Método para establecer conexión a la base de datos
+    try:
+        if conn is not None:
+            cursor = conn.cursor()
+            cursor.execute("SELECT id, nombre, gerente FROM departamento")
+            departamentos = cursor.fetchall()  # Recupera todos los registros
+
+            if departamentos:
+                for departamento in departamentos:
+                    print(f"ID: {departamento[0]}, Nombre: {departamento[1]}, Gerente: {departamento[2]}")
+            else:
+                print("No hay departamentos registrados.")
+    except Exception as e:
+        print(f"Error al mostrar departamentos: {e}")
+    finally:
+        cursor.close()
+        conn.close()
