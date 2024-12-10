f0cd3b25352d650563307b8c92bc95bb15f0625f
susana0994 <susypao09@gmail>
2024-12-10 20:13:40 -0300

    cambio menus 2

--- Main.py
+++ Main.py
@@ -1,6 +1,21 @@
-
+from vista.Vista_menu import menu_principal
 from controlador.Controlador_autenticacion import hay_usuarios,autenticar
 from vista.Vista_usuario import add_user
 
-print("hola")
-    
+
+while True:
+    print("hola")
+    if hay_usuarios():
+        print("Inicio sesión")
+        nombre=input("Ingrese nombre: ")
+        passwd=input("Ingrese contraseña: ")
+        usuario=autenticar(nombre,passwd)
+        if usuario is not None:
+            print(f"Bienvenido {usuario[1]}")
+            menu_principal()
+            break
+        else:
+            print("Usuario o contraseña incorrecta.")
+    else:
+        add_user()
+
